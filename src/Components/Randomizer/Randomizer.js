c6cd39717619ae23e353f43a802f9427cb4ff122
Ahmad Bilal <bilal_290995@yahoo.com>
2020-09-13 11:31:48 +0700

    Upgrade all dependecies

--- src/Components/Randomizer/Randomizer.js
+++ src/Components/Randomizer/Randomizer.js
@@ -6,44 +6,76 @@ import styles from './Randomizer.module.css';
 
 class Randomizer extends Component {
     state = {
-        note: "A"
+        note: "A",
+        bpm: 0
     }
+
+    // notes = [
+    //     "C", "C#",
+    //     "D", "Db", "D#",
+    //     "E", "Eb",
+    //     "F", "F#",
+    //     "G", "Gb", "G#",
+    //     "A", "Ab", "A#",
+    //     "B", "Bb",
+    // ]
+
     notes = [
-        "C", "C#",
-        "D", "Db", "D#",
-        "E", "Eb",
-        "F", "F#",
-        "G", "Gb", "G#",
-        "A", "Ab", "A#",
-        "B", "Bb",
+        "C#",
+        "Db", "D#",
+        "Eb",
+        "F#",
+        "Gb", "G#",
+        "Ab", "A#",
+        "Bb",
     ]
+
+    bpm = 60;
     interval;
 
-    randomize = (bpm) => {
+    randomize = () => {
         return setInterval(() => {
             // console.log(this.notes[Math.floor(Math.random() * 17)]);
             this.setState({
-                note: this.notes[Math.floor(Math.random() * 17)]
+                // note: this.notes[Math.floor(Math.random() * 17)]
+                note: this.notes[Math.floor(Math.random() * 10)]
             })
-        }, bpm);
+        }, (60 / this.bpm) * 1000);
     }
 
     componentDidMount = () => {
-        
+
     }
 
     startHandler = () => {
-        this.interval = this.randomize(1000);
+        this.interval = this.randomize(this.bpm);
     }
 
     stopHandler = () => {
         clearInterval(this.interval);
     }
 
+    changeHandler = (event) => {
+        // console.log(event.target.value);
+        // if (event.target.value < 60) {
+        //     this.bpm = 30;
+        // }
+        // else {
+        //     this.bpm = event.target.value;
+        // }
+        this.bpm = event.target.value;
+        clearInterval(this.interval);
+        this.interval = this.randomize();
+    }
+
     render() {
         return (
             <div className={styles.randomizer}>
                 {/* <h1 className={styles.title}>Randomizer</h1> */}
+                <form className={styles.form}>
+                    <label>BPM</label>
+                    <input className={styles.inputBpm} type="number" onChange={this.changeHandler} />
+                </form>
                 <div className={styles.note}>
                     <Note note={this.state.note} />
                 </div>
